faf87832b86ba3354420ddf45fdf1de156642e5f
ERSUCC <isaacbrown5763@gmail.com>
2025-06-23 14:47:53 -0500

    Correctly generate temporary files

--- src/test/Tests.scala
+++ src/test/Tests.scala
@@ -1,5 +1,7 @@
 package org.nlogo.extensions.table
 
+import org.scalatest.BeforeAndAfterAll
+
 import java.io.File
 import org.nlogo.headless.TestLanguage
 
@@ -8,6 +10,17 @@ object Tests {
   val testFiles     = testFileNames.map( (f) => (new File(f)).getCanonicalFile )
 }
 
-class Tests extends TestLanguage(Tests.testFiles) {
+class Tests extends TestLanguage(Tests.testFiles) with BeforeAndAfterAll {
   System.setProperty("org.nlogo.preferHeadless", "true")
+
+  override def afterAll() {
+    val file = new File("tmp/table")
+    def deleteRec(f: File) {
+      if (f.isDirectory) {
+        f.listFiles().foreach(deleteRec)
+      }
+      f.delete()
+    }
+    deleteRec(file)
+  }
 }
